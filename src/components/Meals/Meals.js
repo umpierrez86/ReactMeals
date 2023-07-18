456904af953d0708187fbcebd1b0414c283361a8
Luciano Umpierrez <umpierrez86@gmail.com>
2023-07-17 21:54:27 -0300

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+import MealsSummary from './MealsSummary.js';
+import AvailableMeals from './AvailableMeals.js';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
